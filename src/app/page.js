a2c82213a68a2380217571d5e4dcc31fd296fd40
sasa-potezica <sasapotezica.contact@gmail.com>
2024-10-07 14:49:58 +0200

    Create Wizard Model

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import bg from "../../public/background/home-background.png";
+import RenderModel from "./components/RenderModel";
+import Wizard from "./components/models/Wizard";
 
 export default function Home() {
   return (
@@ -7,8 +9,15 @@ export default function Home() {
       <Image
         src={bg}
         alt="background-image"
-        className="w-full h-full object-cover object-center"
+        fill
+        className="-z-50 w-full h-full object-cover object-center filter brightness-50"
       />
+      <div className="w-full h-screen">
+        {/* navigation & 3d model */}
+        <RenderModel>
+          <Wizard />
+        </RenderModel>
+      </div>
     </main>
   );
 }
